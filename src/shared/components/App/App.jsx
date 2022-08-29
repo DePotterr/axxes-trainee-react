801038cf416ab558cbf481c71d8611621aabeda1
Nick Lauryssen <nick.lauryssen@axxes.com>
2022-08-29 23:52:29 +0200

    chore: add ex8 solution

--- src/shared/components/App/App.jsx
+++ src/shared/components/App/App.jsx
@@ -1,5 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import MeetingOverview from "../../../pages/MeetingOverview";
+import NotFound from "../../../pages/NotFound";
 
 import theme from "../../css/theme";
 import Layout from "../Layout";
@@ -7,8 +10,19 @@ import Layout from "../Layout";
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Layout>Welcome to the React Traineeship!</Layout>
+      <BrowserRouter>
+        <CssBaseline />
+        <Layout>
+          <Routes>
+            <Route path="/meetings" element={<MeetingOverview />} />
+            <Route
+              path="/"
+              element={<Navigate to="/meetings" replace={true} />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
