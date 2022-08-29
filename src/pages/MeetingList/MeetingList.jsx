e7913e2465dfdbf2d021ad54923d9eec4b2cc90b
Nick Lauryssen <nick.lauryssen@axxes.com>
2022-08-29 23:07:08 +0200

    chore add ex5

---
+++ src/pages/MeetingList/MeetingList.jsx
@@ -0,0 +1,71 @@
+import { Container, Grid, Paper, Typography } from "@mui/material";
+
+// const MEETINGS = [
+//   {
+//     id: 1,
+//     type: "evolution",
+//     date: "30/12/2022",
+//     duration: "1",
+//     invitees: ["Bob"],
+//   },
+//   {
+//     id: 2,
+//     type: "evolution",
+//     date: "30/12/2022",
+//     duration: "1",
+//     invitees: ["Tom"],
+//   },
+//   {
+//     id: 3,
+//     type: "recap",
+//     date: "30/12/2022",
+//     duration: "1",
+//     invitees: ["Lisa", "Bert"],
+//   },
+//   {
+//     id: 4,
+//     type: "recap",
+//     date: "30/12/2022",
+//     duration: "1",
+//     invitees: ["Koen"],
+//   },
+//   {
+//     id: 5,
+//     type: "followUp",
+//     date: "30/12/2022",
+//     duration: "1",
+//     invitees: ["Andrea"],
+//   },
+// ];
+
+const MeetingList = () => {
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={2}>
+        <Grid item container spacing={2}>
+          <Grid item xs={12}>
+            <Paper variant="outlined" sx={{ p: 2 }}>
+              <Grid
+                container
+                justify="space-between"
+                alignItems="center"
+                wrap="nowrap"
+              >
+                <Grid container item direction="column">
+                  <Grid>
+                    <Typography>Type: referral</Typography>
+                    <Typography>Duration: 2 hours</Typography>
+                    <Typography>Date: 20/12/2022</Typography>
+                    <Typography>Invitees: Jens, Marie</Typography>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default MeetingList;
