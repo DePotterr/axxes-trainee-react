762b221df957b8036bab072c7fa924f1d56dd938
Robin de Potter <robin.depotter@axxes.com>
2022-08-30 11:08:17 +0200

    solution ex2

--- src/shared/components/Layout/Layout.jsx
+++ src/shared/components/Layout/Layout.jsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 import { node } from "prop-types"
 import { Toolbar } from "@mui/material"
 import Drawer from "shared/components/Drawer"
@@ -6,14 +6,18 @@ import AppBar from "shared/components/AppBar"
 import { Content } from "./Layout.style"
 
 const Layout = ({ children }) => {
-  const open = true
+  const [open, setOpen] = useState(true)
+
+  const toggleDrawer = () => {
+    setOpen((open) => !open)
+  }
 
   return (
     <Fragment>
-      <AppBar open={open} />
+      <AppBar open={open} onMenuClick={toggleDrawer} />
       <div>
         <Toolbar />
-        <Drawer open={open} />
+        <Drawer open={open} onDrawerClose={toggleDrawer} />
         <Content open={open}>{children}</Content>
       </div>
     </Fragment>
